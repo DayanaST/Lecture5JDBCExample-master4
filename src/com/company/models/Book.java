534a1c9d280bbfe892a0716e57a752c0621cae02
Dayana <dayana.yergaliyeva08@gmail.com>
2026-01-18 02:29:26 +0500

    second commit

---
+++ src/com/company/models/Book.java
@@ -0,0 +1,26 @@
+package com.company.models;
+
+public class Book {
+    private int id;
+    private String title;
+    private int authorId;
+    private String authorName;
+
+    public Book(int id, String title, int authorId) {
+        this.id = id;
+        this.title = title;
+        this.authorId = authorId;
+    }
+
+    public void setAuthorName(String authorName) { this.authorName = authorName; }
+
+    @Override
+    public String toString() {
+        return "Book: " + title + " | Author: " + (authorName != null ? authorName : "ID " + authorId);
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+}
+
---
+++ src/com/company/repositories/BookRepository.java
@@ -0,0 +1,53 @@
+package com.company.repositories;
+import com.company.data.interfaces.IDB;
+import com.company.models.Book;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookRepository {
+    private final IDB db;
+
+    public BookRepository(IDB db) { this.db = db; }
+
+    public List<Book> getAllBooks() {
+        List<Book> books = new ArrayList<>();
+        String sqlBooks = "SELECT book_id, title,author_id FROM books";
+        try (Connection con = db.getConnection();
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(sqlBooks)) {
+            while (rs.next()) {
+                Book book = new Book(
+                        rs.getInt("book_id"),
+                        rs.getString("title"),
+                        rs.getInt("author_id")
+                );
+                books.add(book);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        for (Book book : books) {
+            book.setAuthorName(getAuthorNameById(book.getAuthorId()));
+        }
+
+        return books;
+    }
+
+    private String getAuthorNameById(int id) {
+        String sql = "SELECT name FROM users WHERE author_id = ?";
+        try (Connection con = db.getConnection();
+             PreparedStatement st = con.prepareStatement(sql)) {
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                return rs.getString("name");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "Unknown";
+    }
+}
+
